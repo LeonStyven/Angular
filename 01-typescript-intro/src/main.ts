3d5b274ac5a256aa89b050a978e9a52914902525
LeonStyven <j.stca03@gmail.com>
2025-02-08 22:00:17 -0500

    Args destructuring

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -3,8 +3,7 @@
 // import './topics/02-object-interface';
 // import './topics/03-functions'
 // import './topics/04-homework-types'
-import './topics/05-basic-destructuring'
-
+import './topics/06-function-destructuring'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = 
 `
   Hola Mundo
