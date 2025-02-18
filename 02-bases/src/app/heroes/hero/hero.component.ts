4d6f9c416230546fdb3fb4b671310600dceb1648
LeonStyven <j.stca03@gmail.com>
2025-02-17 21:59:42 -0500

    Angular If

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -11,4 +11,27 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero():void {
+    //TODO: create Hero NAME changer
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void {
+    //TODO: create Hero AGE changer
+    this.age = 30;
+  }
+
+  resetForm():void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+
 }
