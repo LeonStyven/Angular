f86ff2e0b6f19f5d19d67e201171786ae3ed2b89
Leon Styven <j.stca03@gmail.com>
2025-03-04 22:10:15 -0500

    Dragonball component homework and pipes

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: CounterPageComponent,
+  },
+
+  {
+    path: 'dragonball',
+    component: DragonballPageComponent,
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
