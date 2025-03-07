1fcce624708a7331583a664961a94adc48774a6c
Leon Styven <j.stca03@gmail.com>
2025-03-06 23:14:34 -0500

    Events emiter from component to Parent

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, computed, signal } from '@angular/core';
+import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 interface Character {
   id: number;
@@ -8,6 +10,11 @@ interface Character {
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
+  imports: [
+    CharacterListComponent,
+    CharacterListComponent,
+    CharacterAddComponent,
+  ],
 })
 export class DragonballSuperPageComponent {
   name = signal('');
@@ -24,19 +31,8 @@ export class DragonballSuperPageComponent {
     };
   });
 
-  addCharacter() {
-    if (!this.name() || !this.power() || this.power() <= 0) {
-      return;
-    }
-
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power(),
-    };
-
+  addCharacter(newCharacter: Character) {
     this.characters.update((list) => [...list, newCharacter]);
-    this.resetFields();
   }
 
   resetFields() {
