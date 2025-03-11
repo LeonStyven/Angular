c68fbce662eca5312b8b36b0ec6e21bfd0ea2bd3
LeonStyven <j.stca03@gmail.com>
2025-03-11 15:15:56 -0500

    Gifs side menu header and options added

--- 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
+import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SideMenuHeaderComponent,
+    SideMenuOptionsComponent
   ],
   templateUrl: './dashboard-page.component.html',
 })
