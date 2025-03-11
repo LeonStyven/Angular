5ad033307d3b2c04c1a4c5a44c2dfdd5b604f186
LeonStyven <j.stca03@gmail.com>
2025-03-11 17:09:11 -0500

    RouterLinks implemented on component childrens

---
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption {
+  icon: string
+  label: string;
+  route: string;
+  sublabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ]
+}
