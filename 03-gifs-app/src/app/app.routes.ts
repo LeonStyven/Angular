031e00c07e7f41af1bde6571fdb2663688141137
Leon Styven <j.stca03@gmail.com>
2025-03-21 23:46:01 -0500

    Add gif history page and update routing to include search history

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -1,33 +1,37 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
 
-        children: [
-            
-            {
-                path: 'trending',
-                loadComponent: () => import('./gifs/pages/trending-page/trending-page.component'),
-            },
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component'),
+      },
+      {
+        path: 'history/:query', //Recibe argumentos en la ruta
+        loadComponent: () =>
+          import('./gifs/pages/gif-history/gif-history.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component'),
+      },
 
-            {
-                path: 'search',
-                loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
-            },
-
-            {
-                path: '**',
-                redirectTo: 'trending',
-            }
-        ]
-    },
-
-
-
-    {
+      {
         path: '**',
-        redirectTo: 'dashboard',
-    }
+        redirectTo: 'trending',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
 ];
