b7e8c10b6297c49ec0f7fd55ccf33ddede71d364
Leon Styven <j.stca03@gmail.com>
2025-03-31 16:21:26 -0500

    chore: update launch configuration to use Microsoft Edge and add commented scroll state records

--- 03-gifs-app/src/app/shared/services/scroll-state.service.ts
+++ 03-gifs-app/src/app/shared/services/scroll-state.service.ts
@@ -3,4 +3,12 @@ import { Injectable, signal } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class ScrollStateService {
   trendingScrollState = signal(0);
+
+  /* pagesScrollState: Record<string, number> = {
+    'Page1' : 1000,
+    'Page2' : 0,
+    'Page3' : 564,
+    'Page4' : 344,
+    'Page5' : 2323,
+  } */
 }
