c6ae9b7af20ed223b26c508917cc71859ca3363a
Leon Styven <j.stca03@gmail.com>
2025-05-17 00:05:04 -0500

    Update routing configuration to include a default route and lazy loading for the country module.

--- 04-country-app/src/app/app.routes.ts
+++ 04-country-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './country/pages/by-capital-page/by-capital-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: ByCapitalPageComponent,
+  },
+
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes'), //.then(m => m.countryRoutes)
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+  }
+
+
+];
