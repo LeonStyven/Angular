5c1f788a8bdcc2067e5df4eadf63271dc547d650
Leon Styven <j.stca03@gmail.com>
2025-05-19 16:29:04 -0500

    feat(country-app): expand routing and enhance by-capital-page functionality
    - Added new routes for by-country and by-region pages.
    - Updated by-capital-page to include a placeholder in the search input component for improved user experience.

--- 04-country-app/src/app/country/country.routes.ts
+++ 04-country-app/src/app/country/country.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
-import { CountryLayoutPageComponent } from './layouts/CountryLayoutPage/CountryLayoutPage.component';
+import { CountryLayoutPageComponent } from './Layouts/CountryLayoutPage/CountryLayoutPage.component';
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { ByCountryPageComponent } from "./pages/by-country-page/by-country-page.component";
+import { ByRegionPageComponent } from "./pages/by-region-page/by-region-page.component";
 
 
 
@@ -13,6 +15,14 @@ export const countryRoutes: Routes = [
       {
         path: 'by-capital',
         component: ByCapitalPageComponent
+      },
+      {
+        path: 'by-country',
+        component: ByCountryPageComponent
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPageComponent
       },
       {
         path: '**',
