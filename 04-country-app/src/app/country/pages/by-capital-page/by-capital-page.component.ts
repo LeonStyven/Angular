e074d9c3b37004b36f0a9f7596fb1f898ed9dc1c
Leon Styven <j.stca03@gmail.com>
2025-05-21 23:55:38 -0500

    feat(country-app): enhance app configuration and by-capital-page functionality
    - Added HttpClient with fetch support to app configuration for improved API interactions.
    - Updated ByCapitalPageComponent to utilize CountryService for searching countries by capital, enhancing search functionality and logging of results.
    - Removed hardcoded navigation button from country list template for cleaner UI.

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,7 +9,15 @@ import { SearchInputComponent } from '../../components/search-input/search-input
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-  searchByCapital(searchValue: string): void {
-    console.log('Buscando por capital:', searchValue);
+
+  CountryService = inject(CountryService);
+
+  searchByCapital(query: string): void {
+    console.log({query});
+
+    this.CountryService.searchByCapital(query)
+    .subscribe(countries => {
+      console.log({countries});
+    })
   }
 }
