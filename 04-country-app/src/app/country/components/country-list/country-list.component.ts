2e6920f95e6988007f02ad07f579178928d691fc
Leon Styven <j.stca03@gmail.com>
2025-08-14 23:16:30 -0500

    fix(country-app): update country list component for implement the More info btn
    - Modified the country list template to use routerLink for navigation to detailed country information.
    - Imported RouterLink in CountryListComponent to support the new navigation feature.

--- 04-country-app/src/app/country/components/country-list/country-list.component.ts
+++ 04-country-app/src/app/country/components/country-list/country-list.component.ts
@@ -1,10 +1,11 @@
 import { DecimalPipe } from '@angular/common';
 import { Country } from '../../interfaces/country.interface';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'country-list',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, RouterLink],
   templateUrl: './country-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
