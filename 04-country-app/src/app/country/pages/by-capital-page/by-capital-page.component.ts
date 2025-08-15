ae4834da338458e26800fb66d4ca686877afd700
Leon Styven <j.stca03@gmail.com>
2025-08-14 22:52:23 -0500

    fix(country-app): update country list and by-capital-page components for improved data handling
    - Renamed the "Nombre" column to "País" in CountryListComponent for better clarity.
    - Enhanced ByCapitalPageComponent to utilize a resource for managing search queries and loading states.
    - Improved error handling by displaying the actual error message from the country resource.

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,45 +1,65 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, inject, input, resource, signal } from '@angular/core';
 import { CountrySearchInputComponent } from "../../components/country-search-input/country-search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
-  standalone: true,
   imports: [CountrySearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
 
-  countryService = inject(CountryService);
+  countryService = inject(CountryService); // Inject the CountryService
+  query = signal(''); // Create a signal to store the search query
 
-  isLoading = signal(false);
-  isError = signal<string|null>(null);
-  countries = signal<Country[]>([])
 
-  onSearch(query: string){
+  //This is when we start this >:c
+  countryResource = resource({
 
-    if(this.isLoading() ) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    
-    
-    this.countryService.searchByCapital(query)
-    .subscribe({
-      next:(countries) => {
-        
+    params: () => ({query: this.query()}), //query de busqueda
+    loader: async( {params} ) => { //recibe the params and return a PROMISE
+      if( !params.query ) return[]; // if not query, return empty array
+      
+      return firstValueFrom( //Convert the observable to a promise
+        this.countryService.searchByCapital(params.query) //makes the search
+      )
+    }
+  })
+
+
+
+
+  /* isLoading = signal(false);
+    isError = signal<string|null>(null);
+    countries = signal<Country[]>([])
+
+    onSearch(query: string){
+
+      if(this.isLoading() ) return;
+      this.isLoading.set(true);
+      this.isError.set(null);
+      
+      
+      this.countryService.searchByCapital(query)
+      .subscribe({
+        next:(countries) => {
+          
+            this.isLoading.set(false);
+            this.countries.set(countries);
+
+        },
+        error: (err) => {
           this.isLoading.set(false);
-          this.countries.set(countries);
+          this.countries.set([]);
+          this.isError.set(err)
+        }
+      })
+      
+    } */
 
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err)
-      }
-    })
-    
-  }
+
 
 }
